1c5b0af0786680fddb861e1d1e696cb05a1ccf3b
Anton Solukov <solukov.anton@gmail.com>
2023-10-03 16:30:26 +0300

    Задачка

---
+++ zadacha6.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5aba780a6a176b029800041c/solutions/javascript?filter=me&sort=best_practice&invalids=false
+function maxMultiple(divisor, bound){
+  let a=divisor
+  let b=bound
+  for (;a<=b;a=a+divisor){
+      
+  }
+  
+  return a-divisor
+  
+  }

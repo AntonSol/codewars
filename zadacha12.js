b9333a606bd78d9e6d853648d3e587a76cdffbc2
Anton Solukov <solukov.anton@gmail.com>
2023-10-13 12:56:09 +0300

    задачка комплид

---
+++ zadacha12.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
+function arrayDiff(a, b) {
+let mass1=[]
+let mass2=[]
+
+  
+for(let k=0;k<b.length;k++){
+   for (let i=0;i<a.length;i++){
+    
+}  
+}
+
+  console.log(mass1)
+}
+
+arrayDiff([1,2,3,4,5],[2,3,4])

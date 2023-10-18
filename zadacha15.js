b7a1bc6927050c0fde61cccf4b5321c13065d1ab
AntonS <solukov.anton@gmail.com>
2023-10-18 21:06:03 +0300

    задачка комплид

--- zadacha15.js
+++ zadacha15.js
@@ -1,12 +1,17 @@
 // https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
 function persistence(num) {
-let peremennya=0
-  let arrayNumber=(''+num).split('').map(Number)
-for (let i=0;i<arrayNumber.length;i++){
-peremennya=arrayNumber[i]*arrayNumber[i+1]
+
+if(num<10){
+  return 0
 }
-console.log(peremennya)
+let arrayNumber=(''+num).split('').map(Number)
+let x=0
+for(;arrayNumber.length>1;){
+peremennya=arrayNumber.reduce((acc, rec)=>acc*rec)
+arrayNumber=(''+peremennya).split('').map(Number);x++}
+
+console.log(x)
 }
 
 
-persistence(999)
+persistence(39)

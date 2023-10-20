d29d342c727b76555f8dfe268250a8feb857f15d
Anton Solukov <solukov.anton@gmail.com>
2023-10-20 12:10:06 +0300

    не комплид

---
+++ zadacha16.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/javascript
+function order(words){
+let nomera=[0,1,2,3,4,5,6,7,8,9]
+let slova=words.split(' ')
+let itog=[]
+for (let i=0;i<slova.length;i++){
+  for (let k=0;k<slova[i].length;k++){
+    if (slova[i][k]==i){
+      itog.push()
+
+    }
+  }
+}
+console.log(slova)
+}
+order ('is2 Thi1s T4est 3a')

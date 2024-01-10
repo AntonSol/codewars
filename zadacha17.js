f52ae9ad4f7814a7a902c87e0945f9fd4efc0bf5
Anton Solukov <solukov.anton@gmail.com>
2024-01-10 14:21:24 +0300

    комплид

--- zadacha17.js
+++ zadacha17.js
@@ -2,15 +2,15 @@
 function order(words){
 let nomera=[0,1,2,3,4,5,6,7,8,9]
 let slova=words.split(' ')
-let itog=[]
+let itog=new Array (slova.length)
 for (let i=0;i<slova.length;i++){
-  for (let k=0;k<slova[i].length;k++){
-    if (slova[i][k]==i){
-      itog.push()
-
+  for(let k=0;k<slova[i].length;k++){
+    if (nomera.includes(parseInt(slova[i][k]))){
+      itog[parseInt(slova[i][k])-1]=slova[i]
+      break
     }
   }
 }
-console.log(slova)
+console.log(itog.join(' '))
 }
-order ('is2 Thi1s T4est 3a')
+order ('')

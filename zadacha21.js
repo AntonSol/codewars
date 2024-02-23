5a1f550b42bb5c20b0b1802189a18820695638f3
AntonS <solukov.anton@gmail.com>
2024-02-23 20:39:22 +0300

    нот комплид

---
+++ zadacha21.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+function positiveSum(arr) {
+let x=0
+  if (arr.length=0){
+  return 0
+} else {
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]>0){x=arr[i]
+    }
+  }
+} console.log(arr)
+}
+
+positiveSum([1,-4,7,12])

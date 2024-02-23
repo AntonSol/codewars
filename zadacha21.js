f3e2a96743ac5d8a6752b1a6971295e89a527a1a
AntonS <solukov.anton@gmail.com>
2024-02-23 21:20:33 +0300

    комплид

--- zadacha21.js
+++ zadacha21.js
@@ -1,14 +1,15 @@
 // https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
 function positiveSum(arr) {
-let x=0
-  if (arr.length=0){
-  return 0
-} else {
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]>0){x=arr[i]
-    }
+  if (arr.length === 0) {
+    return 0;
   }
-} console.log(arr)
-}
 
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      x = x + arr[i];
+    }
+  } 
+  return x;
+}
 positiveSum([1,-4,7,12])
